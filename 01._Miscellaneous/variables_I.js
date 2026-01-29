abde4ecbcebb48cc9de84668549432277b616ee2
anderslatif <anderslatif@users.noreply.github.com>
2026-01-29 10:32:57 +0100

    Added the variables_I file

---
+++ 01._Miscellaneous/variables_I.js
@@ -0,0 +1,37 @@
+
+// Node REPL
+
+// type coercion
+// strict equality
+
+
+// Rule 1: use const per default unless it needs to change, then use let
+
+// const is constant in the assignment
+
+const luckyNumber = 4;
+
+console.log(luckyNumber);
+
+// JS Data types
+// String, Boolean, Number, BigInt, Object, Symbol, null, undefined 
+
+const person = {
+    // key-value pair    
+    name: "Matias"
+};
+
+person.name = "Gustav";
+
+person.age = 10021132134;
+
+console.log(person);
+
+const people = [];
+
+people.push("Victor");
+
+console.log(people);
+
+// ASI
+
